db85db10e521b310eaf3de6e09b860e3bf8cc789
AQDEV <uzwebdevolopersn2@gmail.com>
2021-09-03 14:47:10 -0400

    Fixed$_GET mode

--- grades.php
+++ grades.php
@@ -1,37 +1,37 @@
 <?php
 include "api.php";
 if (array_key_exists("sessionkey", $_GET)) {
+    $api = new api([
+        "sessionkey" => $_GET["sessionkey"],
+    ]);
+    $api->getUsername();
+    $res = $api->getAllGrades();
     if (!array_key_exists("mode", $_GET)) {
-        $api = new api([
-            "sessionkey" => $_GET["sessionkey"],
-        ]);
-        $api->getUsername();
-        $res = $api->getAllGrades();
-        if (!array_key_exists("mode", $_GET)) {
-            header("Content-type:application/json");
-            echo $api->arrayToJson($res);
-        } else {
-            switch ($_GET['mode']) {
-                case 'PDF':
-                    header(
-                        "Location: {$CFG->host}/grades_mode_pdf.php"
-                    );
-                    break;
-                case "XLS":
-                    header(
-                        "Location: {$CFG->host}/grades_mode_xls.php"
-                    );
-                    break;
-                case 'HTML':
-                    header(
-                        "Location: {$CFG->host}/grades_mode_html.php"
-                    );
-                    break;
-                default:
-                    # code...
-                    break;
-            }
-
+        header("Content-type:application/json");
+        echo $api->arrayToJson($res);
+    } else {
+        switch ($_GET['mode']) {
+            case 'PDF':
+                header(
+                    "Location: {$CFG->host}/grades_mode_pdf.php"
+                );
+                echo $CFG->host;
+                break;
+            case "XLS":
+                header(
+                    "Location: {$CFG->host}/grades_mode_xls.php"
+                );
+                break;
+            case 'HTML':
+                header(
+                    "Location: {$CFG->host}/grades_mode_html.php"
+                );
+                break;
+            default:
+                # code...
+                break;
         }
+
     }
+
 }
